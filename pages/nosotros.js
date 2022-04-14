696ecd092fc9b4786b78f95424a6a16735d1404c
Jhan Bayer <jhanbayer@gmail.com>
2022-04-14 13:18:00 -0400

    adding main layout

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import Layout from "../components/Layout";
+
+const Nosotros = () => {
+  return (
+    <Layout pagina="Nosotros">
+      <h1>Desde Nosotros</h1>
+      <Link href="/">Ir a inicio</Link>
+    </Layout>
+  );
+};
+
+export default Nosotros;
