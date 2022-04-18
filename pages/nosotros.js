b4c5fea05ca2a363506bb04c56dc86a7efb61480
Jhan Bayer <jhanbayer@gmail.com>
2022-04-18 11:37:41 -0400

    page nosotros added

--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,9 +1,50 @@
+import Image from "next/image";
 import Layout from "../components/Layout";
+import styles from "../styles/Nosotros.module.css";
 
 const Nosotros = () => {
   return (
     <Layout pagina="Nosotros">
-      <h1>Desde Nosotros</h1>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+        <div className={styles.contenido}>
+          <Image
+            layout="responsive"
+            width={600}
+            height={450}
+            src="/img/nosotros.jpg"
+            alt="Imagen de Nosotros"
+          />
+          <div>
+            <p>
+              Sed magna in esse. Diam consetetur justo ipsum tempor sit invidunt
+              takimata. Nonumy nonumy suscipit voluptua ut magna magna diam diam
+              hendrerit stet est rebum. Feugait sit ullamcorper eos ut dolor
+              praesent tempor feugait vel ea dignissim labore eum labore eirmod
+              eum. Suscipit sanctus molestie dolor soluta. Zzril in voluptua.
+              Feugait ipsum laoreet accusam magna aliquyam consetetur ipsum
+              lorem eos sit sanctus clita ipsum. Eos sed lorem hendrerit
+              ullamcorper vero sit amet dolor et no kasd eum ut ut facilisis et.
+              Ipsum autem te gubergren quod takimata sadipscing amet et sea et
+              ea duis tempor blandit et sed lorem amet. In dignissim no lorem at
+              iusto eos. Diam amet dolore sit cum magna diam ut lorem.
+            </p>
+            <p>
+              Sed magna in esse. Diam consetetur justo ipsum tempor sit invidunt
+              takimata. Nonumy nonumy suscipit voluptua ut magna magna diam diam
+              hendrerit stet est rebum. Feugait sit ullamcorper eos ut dolor
+              praesent tempor feugait vel ea dignissim labore eum labore eirmod
+              eum. Suscipit sanctus molestie dolor soluta. Zzril in voluptua.
+              Feugait ipsum laoreet accusam magna aliquyam consetetur ipsum
+              lorem eos sit sanctus clita ipsum. Eos sed lorem hendrerit
+              ullamcorper vero sit amet dolor et no kasd eum ut ut facilisis et.
+              Ipsum autem te gubergren quod takimata sadipscing amet et sea et
+              ea duis tempor blandit et sed lorem amet. In dignissim no lorem at
+              iusto eos. Diam amet dolore sit cum magna diam ut lorem.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
