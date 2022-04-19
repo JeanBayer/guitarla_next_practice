9c0096d587df0ab2eae6a83b469c3c6f8050c837
Jhan Bayer <jhanbayer@gmail.com>
2022-04-19 15:40:10 -0400

    fetching guitarras

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,12 +1,25 @@
 import React from "react";
 import Layout from "../components/Layout";
 
-const Tienda = () => {
+const Tienda = ({ guitarras }) => {
   return (
     <Layout pagina="Tienda Virtual">
-      <h1>Desde Tienda</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra colección</h1>
+      </main>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarras`;
+  const respuesta = await fetch(url);
+  const guitarras = await respuesta.json();
+  return {
+    props: {
+      guitarras,
+    },
+  };
+}
 
 export default Tienda;
