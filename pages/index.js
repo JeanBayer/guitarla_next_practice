7fb2535fab235e865e16c3dbcd84f7928cfa21f9
Jhan Bayer <jhanbayer@gmail.com>
2022-04-20 16:24:09 -0400

    feat: fetch the guitars in the index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,24 @@
 import Layout from "../components/Layout";
+import Listado from "../components/Listado";
 
-export default function Home() {
+export default function Home({ guitarras }) {
   return (
     <Layout pagina="Inicio">
-      <h1 className="heading">Aprendiendo con Next</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuesta colección</h1>
+        <Listado guitarras={guitarras} />
+      </main>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitarrras`;
+  const respuesta = await fetch(url);
+  const guitarras = await respuesta.json();
+  return {
+    props: {
+      guitarras,
+    },
+  };
 }
