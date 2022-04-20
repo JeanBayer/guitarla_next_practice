b6bc003ee88f764b4e12e53efeb3eaf6ce116d66
Jhan Bayer <jhanbayer@gmail.com>
2022-04-20 16:15:12 -0400

    feat: add form in guitar's page

--- pages/guitarras/[url].js
+++ pages/guitarras/[url].js
@@ -16,8 +16,22 @@ const Producto = ({ guitarra }) => {
         ></Image>
         <div className={styles.contenido}>
           <h3>{nombre}</h3>
-          <p className={styles.descripcion}>{descripcion}</p>
+          <p>{descripcion}</p>
           <p className={styles.precio}>${precio}</p>
+          <form className={styles.formulario}>
+            <label>Cantidad:</label>
+            <select>
+              <option value="">-- Seleccione --</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+            </select>
+            <input type="submit" value="Agregar al carrito" />
+          </form>
         </div>
       </div>
     </Layout>
