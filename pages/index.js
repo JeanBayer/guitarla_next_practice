09b47bfdd5e78bd5188b160812b17f8a87ff392e
Jhan Bayer <jhanbayer@gmail.com>
2022-04-28 12:48:59 -0400

    feat: added the blogs in the index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Layout from "../components/Layout";
 import Listado from "../components/Listado";
 import Curso from "../components/Curso";
+import ListadoBlog from "../components/ListadoBlog";
 
-export default function Home({ guitarras, curso }) {
+export default function Home({ guitarras, curso, entradas }) {
+  console.log(entradas);
   return (
     <Layout pagina="Inicio">
       <main className="contenedor">
         <h1 className="heading">Nuestra colección</h1>
         <Listado guitarras={guitarras} />
       </main>
-      <Curso curso={curso}></Curso>
+      <Curso curso={curso} />
+      <section className="contenedor">
+        <ListadoBlog entradas={entradas} />
+      </section>
     </Layout>
   );
 }
@@ -17,21 +22,25 @@ export default function Home({ guitarras, curso }) {
 export async function getServerSideProps() {
   const urlGuitarras = `${process.env.API_URL}/guitarrras`;
   const urlCursos = `${process.env.API_URL}/cursos`;
+  const urlBlog = `${process.env.API_URL}/blogs?_limit=3&_sort=created_at:desc`;
 
-  const [resGuitaras, resCursos] = await Promise.all([
+  const [resGuitaras, resCursos, resBlog] = await Promise.all([
     fetch(urlGuitarras),
     fetch(urlCursos),
+    fetch(urlBlog),
   ]);
 
-  const [guitarras, curso] = await Promise.all([
+  const [guitarras, curso, entradas] = await Promise.all([
     resGuitaras.json(),
     resCursos.json(),
+    resBlog.json(),
   ]);
 
   return {
     props: {
       guitarras,
       curso,
+      entradas,
     },
   };
 }
