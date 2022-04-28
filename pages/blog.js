7a51580cfa28701219ab3b4cb8b4619818b9d8b4
Jhan Bayer <jhanbayer@gmail.com>
2022-04-28 12:48:37 -0400

    feat: extract the component

--- pages/blog.js
+++ pages/blog.js
@@ -1,18 +1,11 @@
-import { useEffect } from "react";
 import Layout from "../components/Layout";
-import Entrada from "../components/Entrada";
-import styles from "../styles/Blog.module.css";
+import ListadoBlog from "../components/ListadoBlog";
 
 const Blog = ({ entradas }) => {
   return (
     <Layout pagina="Blog Virtual">
       <main className="contenedor">
-        <h2 className="heading">Blog</h2>
-        <div className={styles.blog}>
-          {entradas.map((entrada) => (
-            <Entrada key={entrada.id} entrada={entrada} />
-          ))}
-        </div>
+        <ListadoBlog entradas={entradas} />
       </main>
     </Layout>
   );
