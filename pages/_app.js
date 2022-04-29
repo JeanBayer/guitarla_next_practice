fde6b6ded75b5d8853a1d55c470c80d409b8d926
Jhan Bayer <jhanbayer@gmail.com>
2022-04-29 15:41:54 -0400

    feat: save the cart in the local storage

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,37 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/normalize.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const [carrito, setCarrito] = useState([]);
 
+  useEffect(() => {
+    const carritoLS = JSON.parse(localStorage.getItem("carrito")) ?? [];
+    if (carritoLS.length !== 0) {
+      setCarrito(carritoLS);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito]);
+
+  const existeProducto = (id) => {
+    return carrito.some((productoAgregado) => productoAgregado.id === id);
+  };
+
+  const actualizarCarrito = (producto) => {
+    return carrito.map((productoAgregado) => {
+      if (productoAgregado.id === producto.id) {
+        productoAgregado.cantidad = producto.cantidad;
+      }
+      return productoAgregado;
+    });
+  };
+
   const agregarCarrito = (producto) => {
-    if (
-      carrito.some((productoAgregado) => productoAgregado.id === producto.id)
-    ) {
-      const carritoActualizado = carrito.map((productoAgregado) => {
-        if (productoAgregado.id === producto.id) {
-          productoAgregado.cantidad = producto.cantidad;
-        }
-        return productoAgregado;
-      });
-      setCarrito(carritoActualizado);
+    if (existeProducto(producto.id)) {
+      setCarrito(actualizarCarrito(producto));
     } else {
       setCarrito([...carrito, producto]);
     }
